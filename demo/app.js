3b59749d2b081fe700b7399a27c6bb2e1233498b
shiyuant <shiyuant@student.unimelb.edu.au>
2022-04-13 13:35:14 +1000

    Add port

--- demo/app.js
+++ demo/app.js
@@ -2,12 +2,13 @@
 const express = require('express')
 // Set your app up as an express app
 const app = express()
+const port = 3000
 // Tells the app to send the string: "Our demo app is working!" when you hit the '/' endpoint.
 app.get('/', (req, res) => {
     res.send('Our demo app is working!')});
 // Tells the app to listen on port 3000 and logs that information to the console.
-app.listen(3000, () => {
-    console.log('Demo app is listening on port 3000!')});
+app.listen(port, () => {
+    console.log('Demo app is listening on http:localhost:' + port)});
 
 app.get('/PatientHome', (req, res) => {
     res.send('patienthome')});
