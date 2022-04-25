4c8c2d4b79788d71fb3f92b7cd04fbd9cfe0d49c
jnniu <jnniu@student.unimelb.edu.au>
2022-04-25 23:51:55 +0800

    1

--- demo/app.js
+++ demo/app.js
@@ -1,28 +1,42 @@
 //Import express//
-const { urlencoded } = require('express');
+//const { urlencoded } = require('express');
 const express = require('express');
 // Set your app up as an express app
 const exphbs = require('express-handlebars'); // include Handlebars module
+//const res = require('express/lib/response');
 
 const app = express();
 const port = 3000;
 
+Patient = require('../demo/models/patients.js')
 // To encode the res body
-app.use(express.json());
-app.use(urlencoded({exphbs : true}))
+// app.use(express.json());
+// app.use(urlencoded({exphbs : true}))
 
 app.engine('hbs', exphbs.engine({      // configure Handlebars
     defaultlayout: 'main',
     extname: 'hbs',
-    helpers: require("./public/js/helpers").helpers,
+    helpers: require("../demo/public/js/helpers.js").helpers,
 }));
 app.set('view engine', 'hbs');   // set Handlebars view engine
 // Tells the app to send the string: "Our demo app is working!" when you hit the '/' endpoint.
 app.get('/', (req, res) => {
     res.render('PatientHome.hbs')});
 // Tells the app to listen on port 3000 and logs that information to the console.
-app.listen(port, () => {
-    console.log('Demo app is listening on http:localhost:' + port)});
+// app.listen(port, () => {
+//     console.log('Demo app is listening on http:localhost:' + port)});
+
+
+const generalRouter = require("../demo/routes/demoRouters.js");
+
+app.use("/general", generalRouter);
+
+app.listen(port, () =>
+  console.log("> Server is up and running on http://localhost:" + port)
+);
+
+// app.get('/record', (req, res) => {
+//     res.render('Patientrecord.hbs')})
 
 // // link to our router
 // const demoRouter = require('./routes/demoRouter');
--- demo/routes/demoRouters.js
+++ demo/routes/demoRouters.js
@@ -2,13 +2,27 @@ const express = require('express')
 // create our Router object
 const demoRouter = express.Router()
 // import demo controller functions
-const demoController = require('../controllers/demoController')
+const demoController = require('../controllers/demoController.js')
 // add a route to handle the GET request for all demo data
-demoRouter.get('/', demoController.getAllDemoData)
+demoRouter.get('/', demoController.getAllPatients)
 // add a route to handle the GET request for one data instance
-demoRouter.post('/recordData',demoController.updateRecord)
+demoRouter.get('/recordData',demoController.renderRecordData)
+demoRouter.post("/recordData", demoController.updateRecord);
 demoRouter.get('/:id', demoController.getDataById)
-
 
 // export the router
 module.exports = demoRouter
+
+
+// const express = require("express");
+// const controller = require("../controllers/demoController.js");
+// const demoRouter = express.Router();
+
+// demoRouter.get("/", controller.getAllPatients);
+// demoRouter.get("/recordData", controller.renderRecordData);
+// demoRouter.post("/recordData", controller.updateRecord);
+// demoRouter.get("/:id", controller.getOnePatient);
+// demoRouter.post("/addPatient", controller.addOnePatient);
+// // demoRouter.get("/recordData", controller.renderRecordData);
+
+// module.exports = demoRouter;
