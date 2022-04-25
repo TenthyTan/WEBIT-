4341a7318d6d46a554a67e00d98a7753022a759f
shiyuant <shiyuant@student.unimelb.edu.au>
2022-04-25 19:37:36 +1000

    add demo

---
+++ demo/models/demoDoctor.js
@@ -0,0 +1,13 @@
+const demoDoctor =[
+
+    {
+        id: 1,
+        name: "Chris",
+        username: "chrisHandsome",
+        yearOfBirth: "1980",
+        role: "doctor",
+        email: "chris@hmail.com",
+        patientIds: [1, 2]
+    }
+]
+module.exports =demoDoctor
