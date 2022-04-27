becbe89674fc7b08b014c4c4f8d9918e41e76bab
shiyuant <shiyuant@student.unimelb.edu.au>
2022-04-27 12:17:29 +1000

    update app.js

--- demo/app.js
+++ demo/app.js
@@ -23,8 +23,7 @@ app.engine('hbs', exphbs.engine({      // configure Handlebars
 }));
 app.set('view engine', 'hbs');   // set Handlebars view engine
 // Tells the app to send the string: "Our demo app is working!" when you hit the '/' endpoint.
-app.get('/', (req, res) => {
-    res.render('PatientHome.hbs')});
+
 // /Tells the app to listen on port 3000 and logs that information to the console.
 // app.listen(port, () => {
 //     console.log('Demo app is listening on http:localhost:' + port)});
@@ -38,11 +37,14 @@ const clinicianRouter = require("../demo/routes/clinicianRouter.js");
 app.use("/patients", patientRouter);
 app.use("/clinicians", clinicianRouter);
 
+app.get('/', (req, res) => {
+    res.render('PatientHome.hbs')});
+
 app.get('/recordData', (req, res) => {
     res.render('Patientrecorddata.hbs')});
 
 app.get('/viewdata', (req, res) => {
-    res.render('allData.hbs')});
+    res.render('Patientviewdata.hbs')});
 
 
 // const generalRouter = require("../demo/routes/demoRouters.js");
