8669b5482d015d6789c84faaf8d9c658c05865d1
TenthyTan <78153900+TenthyTan@users.noreply.github.com>
2022-04-28 22:23:13 +1000

    Delete demo directory

--- demo/routes/clinicianRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-// create our Router object
-const clinicianRouter = express.Router()
-// import demo controller functions
-const demoController = require('../controllers/demoController.js')
-
-
-// clinicians dashboard
-//demoRouter.get('/dashboard', getAllPatients);
