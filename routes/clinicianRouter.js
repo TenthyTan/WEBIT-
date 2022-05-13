a35bb1bffc632653313fbb372a480c1ee18b5897
HonWrld999 <101167978+HonWrld999@users.noreply.github.com>
2022-05-14 03:39:20 +1000

    Delete the change in routes and controllers

--- routes/clinicianRouter.js
+++ routes/clinicianRouter.js
@@ -4,7 +4,6 @@ const clinicianRouter = express.Router()
 // import demo controller functions
 const patientController = require('../controllers/PatientController.js')
 const clinicianController = require('../controllers/ClinicianController.js')
-
 const passport = require('passport')
 // import login
 const sign = require('./login.js')
@@ -16,6 +15,8 @@ clinicianRouter.get('/dashboard', patientController.getAllRecords);
 clinicianRouter.get('/home', clinicianController.renderHome);
 
 clinicianRouter.get('/login', sign.unLoginDoctor, clinicianController.renderLoginPage)
+
+clinicianRouter.get('/account',sign.unLoginDoctor, clinicianController.renderAccount)
 
 clinicianRouter.post('/login',
                     sign.unLoginDoctor,
