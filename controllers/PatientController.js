2a50239d8c1b7ea5a88f85599440bc98186b69cb
HonWrld999 <101167978+HonWrld999@users.noreply.github.com>
2022-05-14 23:08:51 +1000

    PatientViewData

--- controllers/PatientController.js
+++ controllers/PatientController.js
@@ -23,26 +23,24 @@ async function initPatient() {
   try {
     // find all document in Patient Collection to findout if it is empty
 
-    const hash = await bcrypt.hash('12345678', 10)
+    const hash = bcrypt.hash("12345678", 10);
     const result = await Patient.find();
     if (result.length == 0) {
       const newPatient = new Patient({
         firstName: "Pat",
         lastName: "wu",
         userName: "Pat",
-        password : hash,
+        password: hash,
         email: "pat@gmail.com",
         doctor: "Chirs",
         yearOfBirth: "1991",
         supportMes: "You are the best",
-        
       });
-      
-      
+
       // save new patient to database
       const patient = await newPatient.save();
       // console.log("-- id is: ", patient.id);
-      
+
       return patient.id;
     } else {
       // find our target patient Pat
@@ -78,12 +76,8 @@ async function initRecord(patientId) {
 }
 
 const getAllPatients = (req, res) => {
-  
-  
   //res.render('Cliniciandashboard.hbs', {record: Record.lean(), Patient: Patient.lean()}) // send data to browser
-}
-
-
+};
 
 const getOnePatient = (req, res) => {
   const patient = data.find((one) => one.id == req.params.id);
@@ -133,14 +127,14 @@ const updateRecord = async (req, res) => {
   try {
     const patientId = await initPatient();
     const recordId = await initRecord(patientId);
-    const record = await Record.findOne({ _id: recordId })
-    const key = req.body.key
-    record.data[key].value = req.body.value
-    record.data[key].comment = req.body.comment
+    const record = await Record.findOne({ _id: recordId });
+    const key = req.body.key;
+    record.data[key].value = req.body.value;
+    record.data[key].comment = req.body.comment;
     // data.value = req.body.value
     // data.comment = req.body.comment
-    record.data[key].status = "Recorded"
-    record.data[key].createdDate = new Date()
+    record.data[key].status = "Recorded";
+    record.data[key].createdDate = new Date();
     // await data.save();
     // Record.findOneAndUpdate({}, {});
     await record.save();
@@ -153,21 +147,20 @@ const updateRecord = async (req, res) => {
 
 // handle request to get one data instance
 const getDataById = (req, res) => {
-    // search the database by ID
-    const data = Record.find(data => data.id === req.params.id)
-        // return data if this ID exists
-        if (data) {
-            res.send(data)
-        } else {
-            // You can decide what to do if the data is not found.
-            // Currently, a 404 response is sent.
-            res.sendStatus(404)
-    } 
-}
+  // search the database by ID
+  const data = Record.find((data) => data.id === req.params.id);
+  // return data if this ID exists
+  if (data) {
+    res.send(data);
+  } else {
+    // You can decide what to do if the data is not found.
+    // Currently, a 404 response is sent.
+    res.sendStatus(404);
+  }
+};
 
-
-const getAllRecords = async(req, res) => {
-  try{
+const getAllRecords = async (req, res) => {
+  try {
     const patientId = await initPatient();
     const recordId = await initRecord(patientId);
     const record = await Record.findOne({ _id: recordId })
@@ -176,27 +169,109 @@ const getAllRecords = async(req, res) => {
         options: { lean: true },
       })
       .lean();
-    
 
-  res.render('Cliniciandashboard.hbs', {record: record}); // send data to browser
-  }catch(err){
+    res.render("Cliniciandashboard.hbs", { record: record }); // send data to browser
+  } catch (err) {
     console.log("error happens ", err);
-
   }
-}
+};
 
 const renderHomePage = async (req, res) => {
-  
   res.render("PatientHome.hbs");
-  
+};
+
+const renderPatientData = async (req, res) => {
+  const record = {
+    recordDate: "2022-05-14",
+    data: {
+      mock: [
+        {
+          date: "2022-03-11",
+          bgl: "424",
+          doit: "3213",
+          weight: 155,
+          exercise: 42343,
+        },
+        {
+          date: "2022-03-12",
+          bgl: "424",
+          doit: "3213",
+          weight: 124,
+          exercise: 42343,
+        },
+        {
+          date: "2022-03-13",
+          bgl: "424",
+          doit: "3213",
+          weight: 152,
+          exercise: 42343,
+        },
+        {
+          date: "2022-03-14",
+          bgl: "424",
+          doit: "3213",
+          weight: 133,
+          exercise: 42343,
+        },
+        {
+          date: "2022-03-15",
+          bgl: "424",
+          doit: "3213",
+          weight: 166,
+          exercise: 42343,
+        },
+        {
+          date: "2022-03-16",
+          bgl: "424",
+          doit: "3213",
+          weight: 169,
+          exercise: 42343,
+        },
+        {
+          date: "2022-03-17",
+          bgl: "424",
+          doit: "3213",
+          weight: 155,
+          exercise: 42343,
+        },
+      ],
+      bgl: {
+        name: "Blood Glucose Level (nmol/L)",
+        status: "Unrecorded",
+        comment: " 123",
+        minThreshold: 0,
+        maxThreshold: 20,
+      },
+      doit: {
+        name: "Insulin Taken (units)",
+        status: "Unrecorded",
+        comment: "432 ",
+        minThreshold: 0,
+        maxThreshold: 3,
+      },
+      weight: {
+        name: "Weight (kg)",
+        status: "Unrecorded",
+        comment: " 423",
+        minThreshold: 60,
+        maxThreshold: 80,
+      },
+      exercise: {
+        name: "Exercise (steps)",
+        status: "Not required",
+        comment: " 42",
+        minThreshold: 500,
+        maxThreshold: 1000,
+      },
+    },
+  };
+
+  res.render("Patientviewdata.hbs", { record: record });
 };
 
 const renderLoginPage = async (req, res) => {
-  
   res.render("Patientslogin.hbs", req.session.flash);
-  
 };
-
 
 module.exports = {
   getAllPatients,
@@ -207,9 +282,6 @@ module.exports = {
   getDataById,
   getAllRecords,
   renderHomePage,
-  renderLoginPage
+  renderLoginPage,
+  renderPatientData,
 };
-
-
-
-
