699f8ef9c5592004f15b5d159efc542b81b9fb9a
jnniu <jnniu@student.unimelb.edu.au>
2022-05-22 21:27:09 +0800

    1

--- models/patients.js
+++ models/patients.js
@@ -22,17 +22,17 @@ const patientSchema = new mongoose.Schema({
             min: {type: Number, default: '1'},
             max: {type: Number,  default: '10'}
         },
-        weight: { 
+        doit: { 
             name :{type: String, default:"Insulin Taken (units)" },
+            check: {type: Boolean, required: true, default: 'true'},
+            min: {type: Number,  default: '1'},
+            max: {type: Number,  default: '15'}
+        },
+        weight: { 
+            name :{type: String, default:"Weight (kg)" },
             check: {type: Boolean, required: true, default: 'true'},
             min: {type: Number,  default: '30'},
             max: {type: Number,  default: '200'}
-        },
-        doit: { 
-            name :{type: String, default:"Weight (kg)" },
-            check: {type: Boolean, required: true, default: 'true'},
-            min: {type: Number,  default: '1'},
-            max: {type: Number,  default: '10'}
         },
         exercise: { 
             name :{type: String, default:"Exercise (steps)" },
